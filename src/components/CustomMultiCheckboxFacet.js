43278bb1e2afba2654e72ea8e3812c0fec3eb9bf
Emmanuel-Develops <itakpeemma@gmail.com>
2023-01-16 18:26:22 +0100

    authors search with keyboard nav keys

--- src/components/CustomMultiCheckboxFacet.js
+++ src/components/CustomMultiCheckboxFacet.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 // import { FacetViewProps } from "./types";
 // import type { FieldValue } from "@elastic/search-ui";
 import mapping from "../config/mapping.json";
+import styles from "./styles.module.scss";
 
 function CustomMultiCheckboxFacet({
   className,
@@ -46,6 +47,82 @@ function CustomMultiCheckboxFacet({
     }
     return `${baseClassName} ${getNewClassName(newClassName)}`;
   }
+
+  const checkboxNavIndex = useRef(null);
+  const savedNavIndex = useRef(0);
+  const searchRef = useRef();
+  const multiCheckboxRef = useRef();
+  const [currentNavigateCheckbox, setcurrentNavigateCheckbox] = useState("");
+
+  useEffect(() => {
+    const multiCheckboxWrapper = multiCheckboxRef.current;
+    const multiCheckboxList =
+      multiCheckboxWrapper && Array.from(multiCheckboxWrapper?.children);
+    const searchInput = searchRef.current;
+    // focus back to search when options changes
+    searchInput && searchInput.focus()
+    
+    let currentCheckboxNavIndex = checkboxNavIndex.current
+    setcurrentNavigateCheckbox("")
+    const handleOptionNavigation = (e) => {
+      const charCode =
+        e.keyCode || (typeof e.which === "number" && e.which) || null;
+
+      switch (charCode) {
+        case 40:
+          // downArrow
+          if (currentCheckboxNavIndex === null) {
+            currentCheckboxNavIndex = 0;
+          } else {
+            // if (multiCheckboxList.length - 1 === currentCheckboxNavIndex) {
+            if (currentCheckboxNavIndex > multiCheckboxList.length - 1) {
+              currentCheckboxNavIndex = 0;
+            } else {
+              currentCheckboxNavIndex += 1;
+            }
+          }
+          break;
+        case 38:
+          // upArrow
+          if (currentCheckboxNavIndex === null) {
+            currentCheckboxNavIndex = multiCheckboxList.length - 1;
+          } else {
+            if (currentCheckboxNavIndex === 0) {
+              currentCheckboxNavIndex = multiCheckboxList.length - 1;
+            } else {
+              currentCheckboxNavIndex -= 1;
+            }
+          }
+          break;
+        case 13:
+          // Enter
+          e.preventDefault();
+          if (currentCheckboxNavIndex === null) currentCheckboxNavIndex = 0
+          const input = multiCheckboxList[currentCheckboxNavIndex] &&
+            multiCheckboxList[currentCheckboxNavIndex].querySelector("input");
+          if (input) {
+            savedNavIndex.current = currentCheckboxNavIndex
+            input.click()
+          }
+          break;
+        default:
+          break;
+      }
+      setcurrentNavigateCheckbox(
+        multiCheckboxList[currentCheckboxNavIndex]?.dataset?.checkbox ?? ""
+      );
+      
+    };
+
+    searchInput &&
+      searchInput.addEventListener("keydown", handleOptionNavigation);
+
+    return () => {
+      searchInput &&
+        searchInput.removeEventListener("keydown", handleOptionNavigation);
+    };
+  }, [options]);
+
   return (
     <fieldset className={appendClassName("sui-facet", className)}>
       <legend className="sui-facet__title">{label}</legend>
@@ -55,15 +132,16 @@ function CustomMultiCheckboxFacet({
           <input
             className="sui-facet-search__text-input"
             type="search"
-            placeholder={searchPlaceholder || "Search"}
+            placeholder={currentNavigateCheckbox || searchPlaceholder || "Search"}
             onChange={(e) => {
               onSearch(e.target.value);
             }}
+            ref={searchRef}
           />
         </div>
       )}
 
-      <div className="sui-multi-checkbox-facet">
+      <div className="sui-multi-checkbox-facet" ref={multiCheckboxRef}>
         {options.length < 1 && <div>No matching options</div>}
         {options?.map((option) => {
           const checked = option.selected;
@@ -74,9 +152,18 @@ function CustomMultiCheckboxFacet({
               htmlFor={`example_facet_${label}${getFilterValueDisplay(
                 option.value
               )}`}
-              className="sui-multi-checkbox-facet__option-label"
+              data-checkbox={getFilterValueDisplay(option.value)}
+              className={`${styles.checkboxLabel} 
+                ${
+                  getFilterValueDisplay(option.value) ===
+                  currentNavigateCheckbox
+                    ? styles.currentNavigatedLabel
+                    : ""
+                } 
+                ${checked ? styles.checked : ""}
+                sui-multi-checkbox-facet__option-label`}
             >
-              <div className="sui-multi-checkbox-facet__option-input-wrapper">
+              <div className={styles.checkbox_input_wrapper}>
                 <input
                   data-transaction-name={`facet - ${label}`}
                   id={`example_facet_${label}${getFilterValueDisplay(
@@ -87,11 +174,9 @@ function CustomMultiCheckboxFacet({
                   checked={checked}
                   onChange={() => (checked ? onRemove(value) : onSelect(value))}
                 />
-                <span className="sui-multi-checkbox-facet__input-text">
-                  {getFilterValueDisplay(option.value)}
-                </span>
+                <span className="">{getFilterValueDisplay(option.value)}</span>
               </div>
-              <span className="sui-multi-checkbox-facet__option-count">
+              <span className={styles.option_count}>
                 {option.count.toLocaleString("en")}
               </span>
             </label>
