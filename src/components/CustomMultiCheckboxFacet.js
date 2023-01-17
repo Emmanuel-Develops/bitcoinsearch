85926f35ceef9676f6f0755115ef21a899f81c46
Emmanuel-Develops <itakpeemma@gmail.com>
2023-01-17 10:50:36 +0100

    hook to control checkbox navigation

--- src/components/CustomMultiCheckboxFacet.js
+++ src/components/CustomMultiCheckboxFacet.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 // import { FacetViewProps } from "./types";
 // import type { FieldValue } from "@elastic/search-ui";
 import mapping from "../config/mapping.json";
+import useCheckboxNavigate from "../hooks/useCheckboxNavigate";
 import styles from "./styles.module.scss";
 
 function CustomMultiCheckboxFacet({
@@ -48,80 +49,10 @@ function CustomMultiCheckboxFacet({
     return `${baseClassName} ${getNewClassName(newClassName)}`;
   }
 
-  const checkboxNavIndex = useRef(null);
-  const savedNavIndex = useRef(0);
   const searchRef = useRef();
   const multiCheckboxRef = useRef();
-  const [currentNavigateCheckbox, setcurrentNavigateCheckbox] = useState("");
 
-  useEffect(() => {
-    const multiCheckboxWrapper = multiCheckboxRef.current;
-    const multiCheckboxList =
-      multiCheckboxWrapper && Array.from(multiCheckboxWrapper?.children);
-    const searchInput = searchRef.current;
-    // focus back to search when options changes
-    searchInput && searchInput.focus()
-    
-    let currentCheckboxNavIndex = checkboxNavIndex.current
-    setcurrentNavigateCheckbox("")
-    const handleOptionNavigation = (e) => {
-      const charCode =
-        e.keyCode || (typeof e.which === "number" && e.which) || null;
-
-      switch (charCode) {
-        case 40:
-          // downArrow
-          if (currentCheckboxNavIndex === null) {
-            currentCheckboxNavIndex = 0;
-          } else {
-            // if (multiCheckboxList.length - 1 === currentCheckboxNavIndex) {
-            if (currentCheckboxNavIndex > multiCheckboxList.length - 1) {
-              currentCheckboxNavIndex = 0;
-            } else {
-              currentCheckboxNavIndex += 1;
-            }
-          }
-          break;
-        case 38:
-          // upArrow
-          if (currentCheckboxNavIndex === null) {
-            currentCheckboxNavIndex = multiCheckboxList.length - 1;
-          } else {
-            if (currentCheckboxNavIndex === 0) {
-              currentCheckboxNavIndex = multiCheckboxList.length - 1;
-            } else {
-              currentCheckboxNavIndex -= 1;
-            }
-          }
-          break;
-        case 13:
-          // Enter
-          e.preventDefault();
-          if (currentCheckboxNavIndex === null) currentCheckboxNavIndex = 0
-          const input = multiCheckboxList[currentCheckboxNavIndex] &&
-            multiCheckboxList[currentCheckboxNavIndex].querySelector("input");
-          if (input) {
-            savedNavIndex.current = currentCheckboxNavIndex
-            input.click()
-          }
-          break;
-        default:
-          break;
-      }
-      setcurrentNavigateCheckbox(
-        multiCheckboxList[currentCheckboxNavIndex]?.dataset?.checkbox ?? ""
-      );
-      
-    };
-
-    searchInput &&
-      searchInput.addEventListener("keydown", handleOptionNavigation);
-
-    return () => {
-      searchInput &&
-        searchInput.removeEventListener("keydown", handleOptionNavigation);
-    };
-  }, [options]);
+  const {currentNavigateCheckbox, toggleRefocus} = useCheckboxNavigate(multiCheckboxRef, searchRef, options)
 
   return (
     <fieldset className={appendClassName("sui-facet", className)}>
@@ -188,7 +119,12 @@ function CustomMultiCheckboxFacet({
         <button
           type="button"
           className="sui-facet-view-more"
-          onClick={onMoreClick}
+          onClick={
+            () => {
+              onMoreClick()
+              toggleRefocus()
+            }
+          }
           aria-label="Show more options"
         >
           + More
